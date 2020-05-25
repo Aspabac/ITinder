f52581ed623b87ff9143c4feb7844fd509c75eca
Daigomizuki <daigo3262@gmail.com>
2020-05-26 00:41:16 +0900

    first commit

--- ITinder/ContentView.swift
+++ ITinder/ContentView.swift
@@ -7,10 +7,30 @@
 //
 
 import SwiftUI
+import Firebase
+import SDWebImageSwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+       
+        ZStack{
+            
+            Color("LightWhite").edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                           
+                TopView()
+                           
+                Spacer()
+                           
+                BottomView()
+                
+            }
+                   
+        }
+           
+        
     }
 }
 
@@ -19,3 +39,117 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct TopView : View {
+    
+    var body: some View{
+        
+        HStack{
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("person").resizable().frame(width: 35, height: 35)
+                
+            }.foregroundColor(.gray)
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                
+                Image(systemName: "flame.fill").resizable().frame(width: 30, height: 35)
+            }.foregroundColor(.blue)
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("chat").resizable().frame(width: 35, height: 35)
+
+            }.foregroundColor(.gray)
+        
+            
+        }.padding()
+    }
+    
+}
+
+struct BottomView : View {
+    
+    var body: some View{
+        
+        HStack{
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("reload").resizable().frame(width: 25, height: 25).padding()
+                
+            }.foregroundColor(.yellow)
+            .background(Color.white)
+            .shadow(radius: 25)
+            .clipShape(Circle())
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("clear").resizable().frame(width: 30, height: 30).padding()
+                
+            }.foregroundColor(.red)
+            .background(Color.white)
+            .shadow(radius: 25)
+            .clipShape(Circle())
+            
+            
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("star").resizable().frame(width: 25, height: 25).padding()
+                
+            }.foregroundColor(.green)
+            .background(Color.white)
+            .shadow(radius: 25)
+            .clipShape(Circle())
+            
+            
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("heart").resizable().frame(width: 25, height: 25).padding()
+                
+            }.foregroundColor(.pink)
+            .background(Color.white)
+            .shadow(radius: 25)
+            .clipShape(Circle())
+            
+            
+            Button(action: {
+                
+            }) {
+                
+                Image("current").resizable().frame(width: 25, height: 25).padding()
+                
+            }.foregroundColor(.purple)
+            .background(Color.white)
+            .shadow(radius: 25)
+            .clipShape(Circle())
+            
+            
+        }
+    }
+    
+}
+
+
