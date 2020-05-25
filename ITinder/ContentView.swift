d0839a04a0155cffd49d794c971a65abdee88c4e
Daigomizuki <daigo3262@gmail.com>
2020-05-25 13:48:06 +0900

    Initial Commit

---
+++ ITinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ITinder
+//
+//  Created by 水木大悟 on 2020/05/25.
+//  Copyright © 2020 水木大悟. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
