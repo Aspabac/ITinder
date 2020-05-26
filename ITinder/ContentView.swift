54a4dcdbd833e9f1f5d16f2ada9676f526c88e91
Daigomizuki <daigo3262@gmail.com>
2020-05-27 00:33:05 +0900

    almost end

--- ITinder/ContentView.swift
+++ ITinder/ContentView.swift
@@ -14,6 +14,7 @@ import SDWebImageSwiftUI
 struct ContentView: View {
     
     @EnvironmentObject var obs : observer
+    @State var showLiked = false
     
     var body: some View {
         
@@ -30,7 +31,7 @@ struct ContentView: View {
             
             VStack {
                            
-                TopView()
+                TopView(show: $showLiked)
                            
                 SwipeView()
                            
@@ -38,9 +39,10 @@ struct ContentView: View {
                 
             }
                    
+        }.sheet(isPresented: $showLiked) {
+            
+           LikedPeople()
         }
-           
-        
     }
 }
 
@@ -51,292 +53,20 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct TopView : View {
-    
-    var body: some View{
-        
-        HStack{
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("person").resizable().frame(width: 35, height: 35)
-                
-            }.foregroundColor(.gray)
-            
-            Spacer()
-            
-            Button(action: {
-                
-            }) {
-                
-                Image(systemName: "flame.fill").resizable().frame(width: 30, height: 35)
-            }.foregroundColor(.green)
-            
-            Spacer()
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("chat").resizable().frame(width: 35, height: 35)
-
-            }.foregroundColor(.gray)
-        
-            
-        }.padding()
-    }
-    
-}
-
-struct BottomView : View {
-    
-    var body: some View{
-        
-        HStack{
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("reload").resizable().frame(width: 25, height: 25).padding()
-                
-            }.foregroundColor(.yellow)
-            .background(Color.white)
-            .shadow(radius: 25)
-            .clipShape(Circle())
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("clear").resizable().frame(width: 30, height: 30).padding()
-                
-            }.foregroundColor(.red)
-            .background(Color.white)
-            .shadow(radius: 25)
-            .clipShape(Circle())
-            
-            
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("star").resizable().frame(width: 25, height: 25).padding()
-                
-            }.foregroundColor(.blue)
-            .background(Color.white)
-            .shadow(radius: 25)
-            .clipShape(Circle())
-            
-            
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("heart").resizable().frame(width: 25, height: 25).padding()
-                
-            }.foregroundColor(.pink)
-            .background(Color.white)
-            .shadow(radius: 25)
-            .clipShape(Circle())
-            
-            
-            Button(action: {
-                
-            }) {
-                
-                Image("current").resizable().frame(width: 25, height: 25).padding()
-                
-            }.foregroundColor(.purple)
-            .background(Color.white)
-            .shadow(radius: 25)
-            .clipShape(Circle())
-            
-            
-        }
-    }
-    
-}
 
 
-struct SwipeView : View{
-    
-    @EnvironmentObject var obser : observer
-    var body: some View{
-        
-        GeometryReader{geo in
-            
-            ZStack{
-                
-                ForEach(self.obser.users){i in
-                    
-                    
-                    SwipeDetailView(name: i.name, age: i.age, image: i.image, height: geo.size.height - 100).gesture(DragGesture()
-                        
-                        .onChanged({ (value) in
-                            
-                            if value.translation.width > 0{
-                                
-                                self.obser.update(id: i, value: value.translation.width, degree: 8)
-                            }
-                            else{
-                               
-                                
-                                self.obser.update(id: i, value: value.translation.width, degree: -8)
-                                
-                            }
-                        }).onEnded({ (value) in
-                            
-                            if i.swipe > 0{
-                                
-                                if i.swipe > geo.size.width / 2 - 80{
-                                    
-                                    self.obser.update(id: i, value: 500, degree: 0)
-                                    
-                                    
-                                }
-                                else{
-                                    
-                                     self.obser.update(id: i, value: 500, degree: 0)
-                                    }
-                            }
-                            else{
-                                    
-                                if -i.swipe > geo.size.width / 2 - 80{
-                                    
-                                    self.obser.update(id: i, value: -500, degree: 0)
-                                    
-                                    
-                                }
-                                else{
-                                    
-                                     self.obser.update(id: i, value: 500, degree: 0)
-                                    }
-                            }
-                            
-                        })
-                    
-                    ).offset(x : i.swipe)
-                    .rotationEffect(.init(degrees: i.degree))
-                    .animation(.spring())
-                }
-            }
-        }
-    }
-}
-
-struct SwipeDetailView : View {
-    
-    var name = ""
-    var age = ""
-    var image = ""
-    var height : CGFloat = 0
-    
-    
-    var body: some View{
-        
-        ZStack{
-            
-            AnimatedImage(url: URL(string: image)!).resizable().cornerRadius(20).padding(.horizontal,15)
-            
-            VStack{
-                
-                Spacer()
-                
-                HStack{
-                    
-                    VStack(alignment: .leading, content: {
-                        
-                        
-                        Text(name).fontWeight(.heavy).font(.system(size:20)).foregroundColor(.white)
-                        Text(age).foregroundColor(.white).foregroundColor(.white)
-                        
-                    })
-                        
-                    Spacer()
-                }
-            }.padding([.bottom,.leading],35)
-            
-            
-        }.frame(height : height)
-    }
-    
-    
-    
-}
-
-struct Loader : UIViewRepresentable {
-    
-    func makeUIView(context: UIViewRepresentableContext<Loader>) -> UIActivityIndicatorView {
-     
-        let indicator = UIActivityIndicatorView(style: .large)
-        indicator.startAnimating()
-        return indicator
-        
-    }
-    
-    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<Loader>) {
-        
-    }
-}
 
 
-class observer : ObservableObject {
-    
-    @Published var users = [datatype]()
-    
-    init() {
-        let db  = Firestore.firestore()
-        
-        db.collection("users").getDocuments { (snap, err) in
-        
-            if err != nil{
-                
-                print((err?.localizedDescription)!)
-                return
-            }
-            
-            for i in snap!.documents{
-                
-                let name = i.get("name") as! String
-                let age = i.get("age") as! String
-                let image = i.get("image") as! String
-                let id = i.documentID
-                
-                self.users.append(datatype(id: id, name: name, image: image, age: age, swipe: 0, degree: 0))
-                
-            }
-            
-        }
-    }
-    
-    func update(id : datatype,value : CGFloat,degree : Double) {
-        
-        for i in 0..<self.users.count{
-            
-            if self.users[i].id == id.id {
-                
-                self.users[i].swipe = value
-                self.users[i].degree = degree
-            }
-        }
-    }
-}
 
-struct datatype : Identifiable {
-    
-    var id: String
-    var name : String
-    var image : String
-    var age : String
-    var swipe : CGFloat
-    var degree : Double
-    
-    
-    
-    
-}
+
+
+
+
+
+
+
+
+//you can see that seen users are not displaying in the list..
+//in general zstack will overlay one view on another view so first
+//index will be shown at last as well as last index will shown will be first..
+//time to clean code..
